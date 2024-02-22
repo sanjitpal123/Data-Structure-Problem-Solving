90da4669dcf00ff682711caaf38e93bbe56a1fc4
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-22 21:07:56 +0530

    Add files via upload

---
+++ Array-TwoSum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var c=0;
+    var j=0;
+    var index=["",""];
+    for(let i=0;i<nums.length;i++)
+    {
+        c=nums[i];
+        j=i-1;
+        while(j>=0)
+        {
+            if(nums[j]+c==target)
+            {
+              return   index=[i,j];
+              break;
+            }
+            else
+            {
+                j--;
+            }
+        }
+
+
+    }
+};
