39982c7f0f5934db24c8813d26ee3501fba175e8
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-25 21:28:47 +0530

    Add files via upload

---
+++ Reverse_Word_Leetcode.js
@@ -0,0 +1,28 @@
+//151. Reverse Words in a String / you go to leetcode and search this question and solve it
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let sp = s.trim().split(' ');
+
+   let start = 0;
+   let end = sp.length - 1;
+
+   while (start < end) {
+       let temp = sp[start];
+       sp[start] = sp[end];
+       sp[end] = temp;
+       start++;
+       end--;
+   }
+   let final=[];
+   for(let i=0;i<sp.length;i++)
+   {
+       if(sp[i]!=='')
+       {
+        final.push(sp[i]);
+       }
+   }
+   return final.join(' ');
+};
