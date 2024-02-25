929d831a76689f5bc8b3d320b482be93d5229faf
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-25 21:26:34 +0530

    Add files via upload

---
+++ move_Zeros_Leetcode.js
@@ -0,0 +1,27 @@
+// Q.283. Move Zeroes , you can go to leetcode and get this question 
+
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let start=0;
+     for(let i=0;i<nums.length;i++)
+     {
+         if(nums[i]!==0 && nums[start]==0)
+         {
+             let temp=nums[i];
+             nums[i]=nums[start];
+             nums[start]=temp;
+             start++;
+         }
+         else if(nums[start]!=0)
+         {
+             start++;
+         }
+     }
+  
+  
+    }
+   
