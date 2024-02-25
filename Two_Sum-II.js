6fe36a3d2a860dae91000d5965d72db0b53ce018
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-25 21:36:08 +0530

    Add files via upload

---
+++ Two_Sum-II.js
@@ -0,0 +1,29 @@
+//167. Two Sum II - Input Array Is Sorted
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    
+    let i=0;
+    let j=numbers.length-1;
+    while(i<j)
+    {
+        if(numbers[i]+numbers[j]==target)
+        {
+            return [i+1,j+1];
+        }
+        else if(numbers[i]+numbers[j]>target)
+        {
+            
+            j--;
+        }
+        else 
+        {
+            i++;
+            
+        }
+    }
+      
+};
